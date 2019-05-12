a6d5a205f377adc7d842302b5b80bdfd623ddba5
faroukcr7 <farouk.benarous26@gmail.com>
2019-05-12 10:31:20 +0200

    model buit but i have to convert the data from strings to floats (numbers ) because the NN does not accept feeding strings

--- demo2.py
+++ demo2.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import Series
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+from sklearn.cross_decomposition import tests
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import urllib.parse
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 class Traffic:
@@ -159,6 +167,9 @@ def LoadNormalTestToCsv():
 
 
 if __name__ == "__main__":
+    # for the winwods users you should change this path
+    LOGDIR = "/tmp/mnist_tutorial/"
+
     print("Loading Normal Data ....")
     dfNormal = LoadNormalDataToCsv()
     print(dataframe)
@@ -175,12 +186,99 @@ if __name__ == "__main__":
 
     print(AllTrafics)
 
+    dataframe = pd.read_csv('Data/demo2/AllTraffic.csv')
+
+    inputX = dataframe.loc[:,
+             ['http', 'Agent', 'Pragma', 'Cache', 'Accept', 'Encoding', ' Charset', 'Language', 'Host', 'Cookie',
+              'Connection']].values
+
+    inputY = dataframe.loc[:, ["Target"]].values
+
+    X_train, X_test, y_train, y_test = train_test_split(inputX, inputY, test_size=0.2, random_state=42)  # splitting data
 
     # Parameters
-    n_input = 13  # features
+    n_input = 11  # features
     n_hidden = 7  # hidden nodes
     n_output = 1  # lables
     learning_rate = 0.001
     training_epochs = 1000000  # simply iterations
     display_step = 10000  # to split the display
     # n_samples = inputY.size  # number of the instances
+
+    sess = tf.Session()
+
+    X = tf.placeholder(tf.float32, name="X")
+    tf.summary.histogram("inputs ", X)
+
+    Y = tf.placeholder(tf.float32, name="output")
+    tf.summary.histogram("outputs ", Y)
+
+    with tf.name_scope("Hidden_Layer"):
+        W1 = tf.Variable(tf.zeros([n_input, n_hidden]), name="W1")
+        tf.summary.histogram("Weights 1", W1)
+        b1 = tf.Variable(tf.zeros([n_hidden]), name="B1")
+        tf.summary.histogram("Biases 1", b1)
+        L2 = tf.nn.sigmoid(tf.matmul(X, W1) + b1)
+        tf.summary.histogram("Activation", L2)
+
+    with tf.name_scope("OutputLayer"):
+        W2 = tf.Variable(tf.zeros([n_hidden, n_output]), name="W2")
+        tf.summary.histogram("Weights 2", W2)
+        b2 = tf.Variable(tf.zeros([n_output]), name="B2")
+        tf.summary.histogram("Biases 2", b2)
+        hy = tf.nn.sigmoid(tf.matmul(L2, W2) + b2)
+        tf.summary.histogram("Output", hy)
+
+    # calculate the coast of our calculations and then optimaze it
+    with tf.name_scope("Coast"):
+        cost = tf.reduce_mean(-Y * tf.log(hy) - (1 - Y) * tf.log(1 - hy))
+        tf.summary.histogram("Cost ", cost)
+
+    with tf.name_scope("Train"):
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        tf.summary.histogram("Optimazer ", optimizer.values())
+
+    with tf.name_scope("accuracy"):
+        answer = tf.equal(tf.floor(hy + 0.1), Y)
+        accuracy = tf.reduce_mean(tf.cast(answer, "float32"))
+        tf.summary.scalar("accuracy", accuracy)
+
+    summ = tf.summary.merge_all()
+    saver = tf.train.Saver()
+    """cost = tf.reduce_sum(tf.pow(y_ - y, 2)) / (2 * n_samples)
+      optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+     """
+    # Initialize variabls and tensorflow session
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter(LOGDIR)
+    writer.add_graph(sess.graph)
+
+    # lets Do  Our real traing
+
+    for i in range(training_epochs):
+        sess.run(optimizer, feed_dict={X: X_train, Y: y_train})
+        # Take a gradient descent step using our inputs and  labels
+
+        # That's all! The rest of the cell just outputs debug messages.
+        # Display logs per epoch step
+
+        if (i) % display_step == 0:
+            cc = sess.run(cost, feed_dict={X: X_train, Y: y_train})
+            print("Training step:", '%04d' % (i), "cost=", "{:.35f}".format(cc))
+            # print("\n  W1=", sess.run(W1), " \n W1=", sess.run(W2),
+            # "\n b1=", sess.run(b1), "b2=", sess.run(b2) )
+
+    print("\n ------------------------------------Optimization "
+          "Finished!------------------------------------------\n")
+    training_cost = cc
+    print("Training cost=", training_cost,
+          "\n W1 = \n", sess.run(W1), "\n W2= \n", sess.run(W2),
+          "\n b1=", sess.run(b1), '\n', "\n b2=", sess.run(b2), '\n')
+
+    answer = tf.equal(tf.floor(hy + 0.1), Y)
+    accuracy = tf.reduce_mean(tf.cast(answer, "float32"))
+    # print(sess.run([hy], feed_dict={X: inputX, Y: inputY}))
+    print("Accuracy: ", accuracy.eval({X: X_test, Y: y_test}, session=sess) * 100, "%")
+    print("final Coast = ", training_cost)
+    print("Parameters  :", "\n learning rate  = ", learning_rate, "\n epoches = ", training_epochs,
+          " \n hidden layers  = ", n_hidden, "\n coast function \n optimazer Adam ")
